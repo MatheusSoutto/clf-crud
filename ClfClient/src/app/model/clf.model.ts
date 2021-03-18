5e946c424a1b871ef2748037679e60bd446a0264
Matheus Soutto <matheussoutto27@gmail.com>
2021-03-17 22:48:21 -0300

    adding Clf model

---
+++ ClfClient/src/app/model/clf.model.ts
@@ -0,0 +1,14 @@
+export class Clf {
+  id: string;
+  client: string;
+  rfcIdentity: string;
+  userId: string;
+  requestDate: Date;
+  method: string;
+  request: string;
+  protocol: string;
+  statusCode: number;
+  responseSize: number;
+  referrer: string;
+  userAgent: string;
+}
