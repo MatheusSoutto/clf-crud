1fb0f98e5d42f30825987950ba8df36f8192ab89
Matheus Soutto <matheussoutto27@gmail.com>
2021-03-18 19:53:20 -0300

    poping dialog on event

--- ClfClient/src/app/view/home/home.component.ts
+++ ClfClient/src/app/view/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ClfFormDialogComponent } from 'src/app/shared/clf-form-dialog/clf-form-dialog.component';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit() {
+  }
+
+  addClf(): void {
+    const dialogRef = this.dialog.open(ClfFormDialogComponent, {
+      minWidth: '400px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
 }
