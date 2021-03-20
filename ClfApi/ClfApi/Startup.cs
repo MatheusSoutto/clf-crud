5e9ddc76e46d813f62fe13f84ff17665ad318b49
Matheus Soutto <matheussoutto27@gmail.com>
2021-03-20 02:35:05 -0300

    allowing any request method

--- ClfApi/ClfApi/Startup.cs
+++ ClfApi/ClfApi/Startup.cs
@@ -38,7 +38,8 @@ namespace ClfApi
                     {
                         builder.WithOrigins("http://127.0.0.1:4200",
                                             "http://localhost:4200")
-                                .AllowAnyHeader();
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
                     });
             });
 
